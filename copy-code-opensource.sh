f4d840a22f842eeeca98ab08a9975d75569166e4
Zhirong Shen <zhirong.shen2601@gmail.com>
2018-08-07 20:41:48 +0800

    rm-additional-files

--- copy-code-opensource.sh
+++ copy-code-opensource.sh
@@ -1,6 +1,9 @@
+make clean
+rm ../caso-1.0.0.tar.gz
 rm -rf ../caso-1.0.0
 mkdir ../caso-1.0.0
 cp *.c *.h Makefile readme.pdf ../caso-1.0.0/
 cp -r Jerasure/ ../caso-1.0.0/
 mkdir ../caso-1.0.0/example-trace
 cp ../msr-traces/wdev_3.csv ../msr-traces/wdev_1.csv ../caso-1.0.0/example-trace/
+tar -czvf ../caso-1.0.0.tar.gz ../caso-1.0.0/ 
